ca991035894bfff4c2d95131d7c603cc823c8f0d
Dipti Nirmale <dipti.nirmale@gmail.com>
2015-05-05 13:59:07 -0700

    Added code to detect cell signal strength

--- app/src/main/java/org/codecarrots/watchstatus/CellSignalStatusService.java
+++ app/src/main/java/org/codecarrots/watchstatus/CellSignalStatusService.java
@@ -1,7 +1,18 @@
 package org.codecarrots.watchstatus;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoCdma;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
+import android.telephony.CellSignalStrengthCdma;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.CellSignalStrengthLte;
+import android.telephony.CellSignalStrengthWcdma;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 /**
@@ -40,12 +51,49 @@ public class CellSignalStatusService extends IntentService {
     }
 
     private void broadcastSignalStatus() {
-        mIntent.putExtra("NOTIFICATION", "Notification from CellSignalStatus");
+        String message = watchSignalStatus(this);
+        mIntent.putExtra("NOTIFICATION", message);
         mIntent.putExtra("ID", "101");
         mIntent.setClass(this, NotificationReceiver.class);
         Log.d(LOGTAG, "Broadcasting Cellular Signal Status");
         sendBroadcast(mIntent);
         Log.d(LOGTAG, "Broadcast sent");
+    }
+
+    private String watchSignalStatus(Context context) {
+        CellSignalStrengthWcdma cssWcdma;
+        CellSignalStrengthGsm cssGsm;
+        CellSignalStrengthCdma cssCdma;
+        CellSignalStrengthLte cssLte;
+
+        TelephonyManager phonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        CellInfo cInfo = (CellInfo) phonyManager.getAllCellInfo().get(0);
+
+        if (cInfo instanceof CellInfoWcdma) {
+            CellInfoWcdma ciWcdma = (CellInfoWcdma) cInfo;
+            cssWcdma = ciWcdma.getCellSignalStrength();
+            return signalStrengthString(cssWcdma.getAsuLevel(), cssWcdma.getDbm());
+        }
+        else if (cInfo instanceof CellInfoGsm) {
+            CellInfoGsm ciGsm = (CellInfoGsm) cInfo;
+            cssGsm = ciGsm.getCellSignalStrength();
+            return signalStrengthString(cssGsm.getAsuLevel(), cssGsm.getDbm());
+        }
+        else if (cInfo instanceof CellInfoCdma) {
+            CellInfoCdma ciCdma = (CellInfoCdma) cInfo;
+            cssCdma = ciCdma.getCellSignalStrength();
+            return signalStrengthString(cssCdma.getAsuLevel(), cssCdma.getDbm());
+        }
+        else if (cInfo instanceof CellInfoLte) {
+            CellInfoLte ciLte = (CellInfoLte) cInfo;
+            cssLte = ciLte.getCellSignalStrength();
+            return signalStrengthString(cssLte.getAsuLevel(), cssLte.getDbm());
+        }
+        return new String("Cellular signal not detected");
+    }
+
+    private String signalStrengthString(int asu, int dbm) {
+        return new String("ASU Level := " + asu + "\t DBM Level := " + dbm);
     }
 
 }
