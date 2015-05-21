4e487a267d9df3fc437cc16247ec9daa5ab7cb94
Dipti Nirmale <dipti.nirmale@gmail.com>
2015-05-21 05:57:34 -0700

    Show message with options in NotificationHandler

--- app/src/main/java/org/codecarrots/watchstatus/NotificationHandlerActivity.java
+++ app/src/main/java/org/codecarrots/watchstatus/NotificationHandlerActivity.java
@@ -3,7 +3,12 @@ package org.codecarrots.watchstatus;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -11,7 +16,7 @@ import android.widget.TextView;
  * This class handles events when user clicks on notification.
  * @author Dipti Nirmale
  */
-public class NotificationHandlerActivity extends Activity {
+public class NotificationHandlerActivity extends AppCompatActivity {
 
     private static final String LOGTAG = "NotificationHandler";
     private static final String NOTIFICATION_ID = "NOTIFICATION_ID";
@@ -22,6 +27,7 @@ public class NotificationHandlerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_handler);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         int id = intent.getIntExtra(NOTIFICATION_ID, -1);
@@ -30,6 +36,28 @@ public class NotificationHandlerActivity extends Activity {
 
         TextView messageView = (TextView) findViewById(R.id.message_view);
         messageView.setText(mMessage);
+
+        Button startBuzzerButton = (Button) findViewById(R.id.start_buzzer_button);
+        Button noThanksButton = (Button) findViewById(R.id.no_thanks_button);
+
+        noThanksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void getMessage(int receivedId) {
